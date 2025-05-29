37955933db60d0fcdae38b327d65d5746e9bd3db
HJiyeong <150315692+HJiyeong@users.noreply.github.com>
2025-05-29 21:12:46 +0900

    Update WebConfig.java

--- src/main/java/com/jobscatch/careernavi/config/WebConfig.java
+++ src/main/java/com/jobscatch/careernavi/config/WebConfig.java
@@ -8,7 +8,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:5173") // React 개발 포트
+             .allowedOrigins(
+                "http://localhost:5173",         // 개발용
+                "https://www.careernavi.kr"      // 배포용
+            )
                 .allowedMethods("GET", "POST", "OPTIONS", "DELETE", "PUT")
                 .allowCredentials(true);
     }
